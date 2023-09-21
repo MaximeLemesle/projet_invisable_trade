4916e5e2cf6019912acc3d7a82836e51a92f6b73
Maxime Lemesle <maxime.lemesle7@gmail.com>
2023-09-21 17:36:18 +0200

    Modification de la page détail produit en ajoutant le php

--- php/details.php
+++ php/details.php
@@ -1,4 +1,8 @@
-<?php require_once './header.php'; ?>
+<?php 
+    require_once './header.php'; 
+    require_once './bdd/enchere.php';
+    require_once './bdd/objet.php';
+?>
 
     <main class="container">
 
@@ -19,35 +23,29 @@
 
             <div class="info-pdt">
 
-                <div class="titre-pdt">
-                    <h1 class="h1">
-                        Nom du produit
-                    </h1>
-                </div>
-
-                <div class="desc-pdt">
-                    <div class="prix-pdt">
-                        350€
-                    </div>
-                    <div class="text-pdt">
-                        <strong>L'Émotion Pure en un Objet Invisible</strong><br>
-                        Laissez-vous emporter dans un voyage unique au cœur de l'extraordinaire avec notre produit invisible exclusif. À première vue, il peut sembler énigmatique, mais dès que vous le découvrirez, vous comprendrez pourquoi il est si spécial.
-                        <br><br>
-                        <strong>La Magie de l'Invisible :</strong><br>
-                        Imaginez un objet qui incarne l'essence même de l'imagination. Notre produit invisible est bien plus qu'une simple absence de forme et de couleur. Il est une toile blanche pour votre créativité, une invitation à explorer votre propre monde de possibilités.
-                        <br><br>
-                        <strong>La Musique de l'Invisible :</strong><br>
-                        Plongez dans un univers sonore mystique. Lorsque vous approchez notre produit invisible de votre oreille, vous entendrez des mélodies célestes, des harmonies apaisantes et des chansons qui semblent venir d'un autre monde.
-                        <br><br>
-                        <strong>L'Art de l'Invisible :</strong><br>
-                        Notre produit invisible est une œuvre d'art en soi. Il évoluera avec vous, reflétant votre humeur, vos pensées et vos rêves. Placez-le dans un endroit spécial de votre maison et laissez-le devenir une pièce maîtresse de votre décor.
+                    <div class="titre-pdt">
+                        <h1 class="h1">
+                            <?php 
+                                echo object_info($_GET['id'])[0][1];
+                            ?>   
+                        </h1>
                     </div>
 
-                    <div class="action-pdt">
-                        <a href="#" class="btn">Ajouter à la wishlist</a>
-                        <a href="#" class="btn-primaire">Enchérir à 350€</a>
+                    <div class="desc-pdt">
+                        <div class="prix-pdt">
+                            <?php echo list_enchere($_GET['id'])[0][3]."€"; ?>   
+                        </div>
+                        <div class="text-pdt">
+                            <?php 
+                                echo object_info($_GET['id'])[0][2];
+                            ?>   
+                        </div>
+    
+                        <div class="action-pdt">
+                            <a href="#" class="btn">Ajouter à la wishlist</a>
+                            <a href="#" class="btn-primaire">Enchérir à <?php echo list_enchere($_GET['id'])[0][3]."€"; ?></a>
+                        </div>
                     </div>
-                </div>
 
             </div>
 
@@ -61,93 +59,29 @@
 
             <div class="line">
 
-                <div class="article">
-                    <div class="image">
-                        <div class="like">
-                            <img src="../assets/images/heart.png" alt="like">
-                        </div>
-                        <div class="cta">
-                            <button class="btn-white">
-                                Enchérir
-                            </button>
-                        </div>
-                    </div>
+                <?php   
+                    foreach (list_enchere() as $ench) {
+                        echo "<div class=\"article\">
+                            <div class=\"image\">
+                                <div class=\"like\">
+                                    <img src=\"../assets/images/heart.png\" alt=\"like\">
+                                </div>
+                                <div class=\"cta\">
+                                    <a href=\"./details.php?id=".$ench[5]."\" class=\"btn-white\">Enchérir</a>
+                                </div>
+                            </div>
 
-                    <div class="description">
-                        <h3 class="h3">
-                            Nom du Produit 123
-                        </h3>
-                        <p class="prix">
-                            350€
-                        </p>
-                    </div>
-                </div>
-
-                <div class="article">
-                    <div class="image">
-                        <div class="like">
-                            <img src="../assets/images/heart.png" alt="like">
-                        </div>
-                        <div class="cta">
-                            <button class="btn-white">
-                                Enchérir
-                            </button>
-                        </div>
-                    </div>
-
-                    <div class="description">
-                        <h3 class="h3">
-                            Nom du Produit 123
-                        </h3>
-                        <p class="prix">
-                            350€
-                        </p>
-                    </div>
-                </div>
-
-                <div class="article">
-                    <div class="image">
-                        <div class="like">
-                            <img src="../assets/images/heart.png" alt="like">
-                        </div>
-                        <div class="cta">
-                            <button class="btn-white">
-                                Enchérir
-                            </button>
-                        </div>
-                    </div>
-
-                    <div class="description">
-                        <h3 class="h3">
-                            Nom du Produit 123
-                        </h3>
-                        <p class="prix">
-                            350€
-                        </p>
-                    </div>
-                </div>
-
-                <div class="article">
-                    <div class="image">
-                        <div class="like">
-                            <img src="../assets/images/heart.png" alt="like">
-                        </div>
-                        <div class="cta">
-                            <button class="btn-white">
-                                Enchérir
-                            </button>
-                        </div>
-                    </div>
-
-                    <div class="description">
-                        <h3 class="h3">
-                            Nom du Produit 123
-                        </h3>
-                        <p class="prix">
-                            350€
-                        </p>
-                    </div>
-                </div>
+                            <div class=\"description\">
+                                <h3 class=\"h3\">
+                                    ".object_info($ench[5])[0][1]."
+                                </h3>
+                                <p class=\"prix\">
+                                    ".$ench[3]."€
+                                </p>
+                            </div>
+                        </div>";
+                    }
+                ?>
 
             </div>
 
