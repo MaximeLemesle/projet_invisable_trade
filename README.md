d203ae0a1e9aed95bfae491f0627f3a5ac1307ff
TomySackebandt <tomy49@laposte.net>
2023-09-22 11:02:34 +0200

    Ajout d'explication

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Invisable_Trade
 
 ## BDD
-Le script pour générer la base de données se trouve dans init.sql
+Le script pour générer la base de données se trouve dans `init.sql`, Le fichier est composé en 2 partie, pour séparer la syntaxe POSTGRESQL et MYSQL.
 
 Les informations pour se connecter à la base de données, doivent être modifiées dans `config.php` qui se trouve dans le dossier `php/bdd`.
--- init.sql
+++ init.sql
@@ -1,3 +1,5 @@
+-- POSTGRES
+
 CREATE TABLE Users(
    id SERIAL ,
    nom VARCHAR(50)  NOT NULL,
@@ -26,6 +28,44 @@ CREATE TABLE Enchere(
    prix_initial NUMERIC(15,2)   NOT NULL,
    id_objet SERIAL  NOT NULL,
    id_users SERIAL  NOT NULL,
+   PRIMARY KEY(id),
+   UNIQUE(id_objet),
+   FOREIGN KEY(id_objet) REFERENCES Objet(id),
+   FOREIGN KEY(id_users) REFERENCES Users(id)
+);
+
+
+--MYSQL
+
+
+CREATE TABLE Users(
+   id BIGINT AUTO_INCREMENT ,
+   nom VARCHAR(50)  NOT NULL,
+   prenom VARCHAR(50)  NOT NULL,
+   email VARCHAR(255)  NOT NULL,
+   password VARCHAR(255)  NOT NULL,
+   cagnote NUMERIC(15,2)  ,
+   PRIMARY KEY(id)
+);
+
+CREATE TABLE Objet(
+   id BIGINT AUTO_INCREMENT,
+   nom VARCHAR(255) ,
+   description VARCHAR(255) ,
+   image VARCHAR(255)  NOT NULL,
+   id_users BIGINT  NOT NULL,
+   PRIMARY KEY(id),
+   FOREIGN KEY(id_users) REFERENCES Users(id)
+);
+
+CREATE TABLE Enchere(
+   id BIGINT AUTO_INCREMENT,
+   date_debut DATE NOT NULL,
+   date_fin DATE NOT NULL,
+   prix_actuel NUMERIC(15,2)  ,
+   prix_initial NUMERIC(15,2)   NOT NULL,
+   id_objet BIGINT  NOT NULL,
+   id_users BIGINT  NOT NULL,
    PRIMARY KEY(id),
    UNIQUE(id_objet),
    FOREIGN KEY(id_objet) REFERENCES Objet(id),
